44aa61003cd1ba49b0184298facae5cc91a92b89
Yoann-Michon <yoann972.wm@outlook.com>
2024-07-01 15:02:27 +0200

    fix marker

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,9 +1,16 @@
 import { MapContainer, TileLayer, Marker } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import icon from "../assets/marker.svg"
 
 interface Props{
   position: [number, number];
 }
+
+const customMarker=L.icon({
+  iconUrl: icon,
+  iconSize: [32, 32],
+})
 
 const MyMap = ({position}:Props) => {
   return (
@@ -12,7 +19,7 @@ const MyMap = ({position}:Props) => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={position}/>
+        <Marker position={position} icon={customMarker}/>
       </MapContainer>
   );
 };
